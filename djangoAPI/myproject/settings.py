2f594c2d6158c02e80bf23e5fc2224670f427418
dikachitheman <dikachi.anosike@gmail.com>
2024-06-24 03:18:48 +0100

    drf ui

--- djangoAPI/myproject/settings.py
+++ djangoAPI/myproject/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'ecommerce.apps.EcommerceConfig',
+    # 'drf-spectacular',
+    'rest_framework',
     'ev',
 ]
 
@@ -128,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_SCHEMA_CLASS' : 'drf-spectacular.openapi.AutoSchema'
+# }
